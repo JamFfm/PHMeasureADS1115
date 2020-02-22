f2faf60a759274582c2c50fd2243e91e885c2c01
JamFfm <Jan.Battermann@t-online.de>
2020-02-23 00:10:01 +0100

    changed line number in result

--- calibration.py
+++ calibration.py
@@ -6,5 +6,5 @@ ph7voltage = input("Put in the measured voltage while producing a shortcut betwe
 ph401voltage = input("Put in the measured voltage while measuring the buffer pH 4.01. Format x.xxx:  ")
 VoltageperPH = round((ph7voltage-ph401voltage)/2.99, 5)
 print ("The Voltage per pH Factor is " + str(VoltageperPH))
-print ("put it in the Formula in Line 93 of __init__.py")
+print ("put it in the Formula in Line 114 of __init__.py")
 print ("(7 + ((" + str(ph7voltage) + "- voltage) / " + str(-1*VoltageperPH) + "))")
